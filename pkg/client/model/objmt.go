393337b2205a4159f069acad38cbca1ea9ffe343
Ben Schumacher <ben.schumacher@dell.com>
2021-07-15 16:16:38 -0500

    [OBSDEF-10099] add missing crr fields

--- pkg/client/model/objmt.go
+++ pkg/client/model/objmt.go
@@ -566,4 +566,7 @@ type StoreReplicationThroughputRto struct {
 
 	// ReplicatedDelta metrics of replicated data in object store per storage classes
 	ReplicatedDelta []StorageClassBasedCountSize `xml:"replicated_delta>storage_class_counts,omitempty"`
+
+	// ReplicatedFailedDelta metrics of failed replicated data in object store per storage classes
+	ReplicatedFailedDelta []StorageClassBasedCountSize `xml:"replicated_failed_delta>storage_class_counts,omitempty"`
 }
